560a0911b7ce129575b429dc88f0e2872801ed72
AMTKMR <amtkmr.1990@hotmail.com>
2022-03-17 19:21:49 +0530

    Update apim_api_deploy.ps1

--- apim_api_deploy.ps1
+++ apim_api_deploy.ps1
@@ -2,23 +2,26 @@
 param (
 
     $swaggerSourceUrl,                              # this is API url from where swagger file will be downloaded 
-    $APIMResourceGroup = "rg-apiplatform-nonprd", 
-    $APIMName = "apim-apiplatform-nonprd",
+    $APIMResourceGroup, 
+    $APIMName,
     $backEndUrl, 
     $swaggerTitle,                                  # This will be reflected in APIM as display name
     $uIUrl,                                         # This is UI URL used for CORS policy 
     $ApiId                                          # This is unique Api Name from APIM where you want to add or update API Operation 
 )
 
+# Download the swagger file from the API URL. 
 curl $swaggerSourceUrl -o swagger.json 
+
+# below command replace title object in swagger file with the parameter passed 
 $jsoncontent = get-content -path .\swagger.json | ConvertFrom-Json 
 $jsoncontent.info.title = $swaggerTitle
 $jsoncontent | ConvertTo-json -Depth 100 |out-file .\swagger.json -force 
 
 $ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $APIMResourceGroup -ServiceName $APIMName
-Import-AzApiManagementApi -Context $ApiMgmtContext -SpecificationPath ".\swagger.json" -Path "external/catalystreactor" -SpecificationFormat OpenApiJson -Serviceurl $backEndUrl -ApiId $ApiId
+Import-AzApiManagementApi -Context $ApiMgmtContext -SpecificationPath ".\swagger.json" -Path "<PathAPIM>" -SpecificationFormat OpenApiJson -Serviceurl $backEndUrl -ApiId $ApiId
 
-
+# Add policy if any 
 $policyString = "
     <policies>
         <inbound>
@@ -51,6 +54,7 @@ Set-AzApiManagementPolicy -Context $ApiMgmtContext -ApiId $ApiId -Policy $Policy
 
 # get the api context 
 $api = Get-AzApiManagementApi -Context $ApiMgmtContext -ApiId $ApiId
-# set subscriptionRequired to false
+
+# set subscriptionRequired to false in case you want to. else you can skip this setting 
 $api.SubscriptionRequired=$false
 Set-AzApiManagementApi -InputObject $api
